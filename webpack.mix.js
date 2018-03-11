fb71c27e26850b594c1a0a55e2b894f293294842
Eduardo <EduardoValdezAguirre@gmail.com>
2018-03-11 16:26:35 -0600

    Inicio del proyecto

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+	'resources/assets/css/bootstrap.min.css'
+	], 'public/css/app.css');
+
+mix.scripts([
+	'resources/assets/js/vue.js',
+	'resources/assets/js/axios.js',
+	'resources/assets/js/app.js',
+	'resources/assets/js/jquery-3.2.1.min.js',
+	'resources/assets/js/popper.min.js',
+	'resources/assets/js/bootstrap.min.js',
+	], 'public/js/app.js');
